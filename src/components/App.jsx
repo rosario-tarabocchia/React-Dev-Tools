7769c470f6cb4b5e69b88542bbbd9cb9f69028b1
Rosario Tarabocchia <84477640+rosario-tarabocchia@users.noreply.github.com>
2022-04-05 18:19:53 -0400

    Lesson Complete

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Card from "./Card";
 import contacts from "../contacts";
+import Avatar from "./Avatar";
 
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
-
+      <Avatar src="https://avatars.githubusercontent.com/u/84477640?v=4" />
       <Card
         name={contacts[0].name}
         img={contacts[0].imgURL}
